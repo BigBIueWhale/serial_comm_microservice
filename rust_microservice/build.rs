6b38cab1246c1c0d8a2338462390c8c854ec4532
Ronen Zyroff <rzyroff@gmail.com>
2024-01-26 10:22:41 -0500

    Move Rust project into subfolder, and kept .proto file in outside folder, as well as the precompiled protoc binaries. This is because a client program- such as a typescript app, will need to access that .proto file as well is the precompiled protoc binary. The things that are shared between the client and the gRPC server now exists at the top level of the project. The thing that are specific to the Rust server side, are inside of the folder rust_microservice

--- rust_microservice/build.rs
+++ rust_microservice/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/serial_api.proto")?;
+    tonic_build::compile_protos("../proto/serial_api.proto")?;
     Ok(())
 }
